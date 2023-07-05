33529fa6c5c7da0acb1b4629b40dccd2b2195343
jace <nic.chiron@gmail.com>
2023-07-05 18:10:27 +0200

    begin playerturn

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ export class Game {
     console.log(`Ordre des personnages : ${this.players[0].name}, ${this.players[1].name}, ${this.players[2].name}, ${this.players[3].name}, ${this.players[4].name}`);
     this.turnLeft = 10;
     this.turn = 1;
+
   }
 
   initializeCharacters() {
@@ -22,7 +23,29 @@ export class Game {
     console.log(`Début du tour ${this.turn}.`);
   }
 
+  playerTurn() {
+    for (let i=0; i < this.players.length - 1; i++) {
+      if (this.players[i].isAlive) {
+        console.log(`C'est à toi de jouer, ${this.players[i].name} !`);
+        let potentialVictim = this.players.filter(player => player.status == "playing");
+        potentialVictim = potentialVictim.filter(player => player.name != this.players[i].name);
+        console.log("Voici tes cibles, appuye sur la touche correspondante pour les faire souffrir !")
+        for (let player = 0; player < potentialVictim.length; player++) {
+          console.log(`${player + 1} - ${potentialVictim[player].name}`);
+        }
+        let ask = "0";
+        while (parseInt(ask) <= 0 && parseInt(ask) > potentialVictim.length) {
+          console.log("Merci de choisir un choix valable !");  
+          let ask = prompt(`choisi ta cible, ${this.players[i].name} !`);
+        }
+        // this.players[i].dealDamage(potentialVictim[ask - 1]);
+        console.log(`${this.players[i].name} attaque ${potentialVictim[ask - 1].name} !`);
+      }
+    }
+  }
+
   skipTurn() {
+    console.log(this.turn);
     if (this.turnLeft > 0) {
       this.turnLeft -= 1;
       this.turn++;
