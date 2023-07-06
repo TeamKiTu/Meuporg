086000a8ed376a895ecf65c46d8dac9d23234b34
vincent <kratos_angel_59@hotmail.fr>
2023-07-06 11:33:22 +0200

    deal damages + receive damages + fix players turn + fix les trucs claqués de Manu

--- js/main_classes/game.js
+++ js/main_classes/game.js
@@ -8,7 +8,7 @@ export class Game {
 
   constructor() {
     this.players = this.initializeCharacters();
-    console.log(`Bienvenue dans le MORTAAAAAAL KOMBAAAAAAAAT !`);
+    console.log(`Bienvenue dans le MꝌⱤTAAAAAAL ꝀꝌMBAAAAAAAAT !`);
     console.log(`Ordre des personnages : ${this.players[0].name}, ${this.players[1].name}, ${this.players[2].name}, ${this.players[3].name}, ${this.players[4].name}`);
     this.turnLeft = 10;
     this.turn = 1;
@@ -21,7 +21,9 @@ export class Game {
   }
 
   startTurn() {
-    console.log(`Début du tour ${this.turn}.`);
+    console.log(` ╔══════════════════╗`);
+    console.log(`﴿║ Début du tour ${this.turn}. ║﴾`);
+    console.log(` ╚══════════════════╝`);
   }
 
   playerTurn() {
@@ -36,23 +38,21 @@ export class Game {
           console.log(`${player + 1} - ${potentialVictim[player].name}`);
         }
 
-        let ask = prompt(`choisi ta cible, ${this.players[i].name} !`)(entrez le chiffre correspondant)`);
+        let ask = prompt(`choisi ta cible, ${this.players[i].name} ! entrez le chiffre correspondant)`);
 
         while (parseInt(ask) <= 0 || parseInt(ask) > potentialVictim.length) {
           alert("Merci de choisir un choix valable !");  
           ask = prompt(`choisi ta cible, ${this.players[i].name} !`);
           console.log(ask);
         }
-
-        // this.players[i].dealDamage(potentialVictim[ask - 1]);
         console.log(`${this.players[i].name} attaque ${potentialVictim[ask - 1].name} !`);
         this.players[i].dealDamage(potentialVictim[ask - 1], this.players[i]);
+        console.log(`﴾════════════════════════════﴿`);
       }
     }
   }
 
   skipTurn() {
-    console.log(this.turn);
     if (this.turnLeft > 0) {
       this.turnLeft -= 1;
       this.turn++;
