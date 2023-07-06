319f5cf41a48d7d90ea8b40dfb4c1a1db89eaf70
vincent <kratos_angel_59@hotmail.fr>
2023-07-06 15:49:50 +0200

    fix attspe calls

--- js/character_classes/fighter.js
+++ js/character_classes/fighter.js
@@ -1,12 +1,14 @@
 import {Character} from "../main_classes/character.js";
 export class Fighter extends Character {
   constructor(hp, hpmax, dmg, mana, manamax, name, attspe) {
-    super(12, 12, 4, 40, 40, "Rocky", "Dark Vision");
-    this.attspe = {manacost:20, dmg:5};
+    super(12, 12, 4, 40, 40, "Rocky");
+    this.attspe = {manacost:20, dmg:5, name:"Dark Vision"};
   }
 
   attackSpe(victim) {
+    console.log(`${this.name} utilise ${this.attspe.manacost} points de mana pour lancer ${this.attspe.name} !`);
+    console.log(`De plus, ${this.name} subira 2 dégâts de moins jusqu'à son prochain tour.`);
     this.mana -= this.attspe.manacost;
-    takeDamage(victim, this, true);
+    this.takeDamage(victim, this, 1);
   }
 }
--- js/main_classes/game.js
+++ js/main_classes/game.js
@@ -66,5 +66,4 @@ export class Game {
       return true;
     }
   }
-
 }
